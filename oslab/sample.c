783a6331edf7740051099de9ebf454a7422083a7
204g1a0507 <94514047+204g1a0507@users.noreply.github.com>
2022-08-30 11:29:38 +0530

    K AJAY Created Sample.c"

---
+++ oslab/sample.c
@@ -0,0 +1,11 @@
+ 
+/* Name Of the Candidate: K Ajay kumar
+   Roll No: 204G1A0507
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 30-08-2022
+   Date of Execution: 30-08-2022
+*/
+#include <stdio.h>
+    int main(){
+	printf("Hello World\n");
+ }
