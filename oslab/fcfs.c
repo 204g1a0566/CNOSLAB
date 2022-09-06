4d86a375b62f117bc5c714804f8b42fd214af3af
204g1a0507 <94514047+204g1a0507@users.noreply.github.com>
2022-09-06 11:34:53 +0530

    Update fcfs.c

--- oslab/fcfs.c
+++ oslab/fcfs.c
@@ -1,4 +1,4 @@
-* Name Of the Candidate: K Ajay kumar
+/* Name Of the Candidate: K Ajay kumar
    Roll No: 204G1A0507
    Title of the Experiment: CPU Scheduling Using FCFS Algorithm
    Date of Creation: 30-08-2022
