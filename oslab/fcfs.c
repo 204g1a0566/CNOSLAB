1f3bebe605be903cfe266ba284aa2fc5b78ffacb
204g1a0507 <94514047+204g1a0507@users.noreply.github.com>
2022-09-06 11:34:10 +0530

    Update fcfs.c

--- oslab/fcfs.c
+++ oslab/fcfs.c
@@ -3,7 +3,8 @@
    Title of the Experiment: CPU Scheduling Using FCFS Algorithm
    Date of Creation: 30-08-2022
    Date of Execution: 30-08-2022
-*/#include<stdio.h>
+*/
+ #include<stdio.h>
 #include<conio.h>
 main()
 {
