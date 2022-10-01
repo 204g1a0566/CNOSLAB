d4751168ae4f41c69036b3a0daccbc1d893767e2
204G1A0507 <204G1A0507@srit.ac.in>
2022-10-01 15:14:28 +0530

    SJF.c successfully executed & output generated

--- OSLAB/SJF.c
+++ OSLAB/SJF.c
@@ -1,16 +1,8 @@
-/* Name Of the Candidate: K Ajay kumar
-   Roll No: 204G1A0507
-   Title of the Experiment: CPU Scheduling Using SJF Algorithm
-   Date of Creation: 06-09-2022*/
-*/
-   
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
-int p[20], bt[20], wt[20], tat[20], i, k, n, temp; float wtavg,
-tatavg;
-//clrscr();
+int p[20], bt[20], wt[20], tat[20], i, k, n, temp; 
+float wtavg,tatavg;
 printf("\nEnter the number of processes -- ");
 
 scanf("%d", &n);
@@ -32,7 +24,8 @@ p[i]=p[k];
 p[k]=temp;
 }
 wt[0] = wtavg = 0;
-tat[0] = tatavg = bt[0]; for(i=1;i<n;i++)
+tat[0] = tatavg = bt[0]; 
+for(i=1;i<n;i++)
 {
 wt[i] = wt[i-1] +bt[i-1];
 tat[i] = tat[i-1] +bt[i];
@@ -43,5 +36,5 @@ printf("\n\t PROCESS \tBURST TIME \t WAITING TIME\t TURNAROUND TIME\n");
 for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", p[i], bt[i], wt[i], tat[i]);
 printf("\nAverage Waiting Time -- %f", wtavg/n);
-printf("\nAverage Turnaround Time -- %f", tatavg/n); //getch();
+printf("\nAverage Turnaround Time -- %f", tatavg/n); 
 }
